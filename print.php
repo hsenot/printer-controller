4c1f02e3e76b8951a58df2a995537a42fa9b6899
/herve Senot <hsenot@gmail.com>
2014-02-27 08:20:54 +0000

    Better layout and escape sequence for autocut

--- print.php
+++ print.php
@@ -10,34 +10,38 @@
 
 	// Template string to print
 	$template = <<<EOF
-123456789012345678901234567890123456789012
+
+
+
+
+
+
 $to_print
-3
-4
-5
-6
-7
-8
-9
-10
-Visit: http://temporary-agency.com
-12
-13
-Your assignment is:
-15
-16
-17
-18
-19
-20
+
+
+
+
+
+
+
+  Visit: http://www.temporary-agency.com
+
+
+
+
+            Your assignment is:
+
 EOF;
 
 	// Putting the template in a file
 	file_put_contents('/tmp/print.txt',$template);
-
+	
 	// Print command
 	shell_exec('lpr /tmp/print.txt');
 
+	// Escape sequence to a
+	shell_exec("echo '\x1b\x69' | lpr -l");	
+	
 	// Removing temporary file
 	shell_exec('rm /tmp/print.txt');
 

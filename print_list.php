aa7d3fd85594ce5edde0cb67ab1df58efbf36862
/herve Senot <hsenot@gmail.com>
2014-03-05 05:00:03 +0000

    Added the print list functionality

---
+++ print_list.php
@@ -0,0 +1,94 @@
+<?php
+
+	// Template string to print
+	$template = <<<EOF
+
+Assignment Bank: 
+(L-R)
+
+
+ Sew something with a double meaning to 
+ create a completely new image
+
+ Ruin the first 10 things you see 
+ spontaneously
+
+ Replicate elastic bands and combine
+
+ Collage an orchestra as a pie chart
+
+ Record nothing once
+
+ Copy something in the size of a book
+
+ Draw a circle inside a square
+
+ Colour sticks intuitively
+
+ Copy currency in black and white
+
+ Reinvent a game of cards like a robot
+
+ Introduce a set of instructions in the 
+ gallery
+
+ Paint a flagpole in 11 seconds
+
+ Find a piece of paper during business 
+ hours
+
+ Replicate 'X' in the wrong order
+
+ Question the economy to start a 
+ conversation
+
+ Spend time thinking about time with your 
+ hands
+
+ Continue the notion of work without 
+ telling anyone
+
+ Observe weeds in pen
+
+ Trial staples as an indulgence
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+EOF;
+
+	// Putting the template in a file
+	file_put_contents('/tmp/print.txt',$template);
+	
+	// Print command
+	shell_exec('lpr /tmp/print.txt');
+
+	// Escape sequence to a
+	shell_exec("echo '\x1b\x69' | lpr -l");	
+	
+	// Removing temporary file
+	shell_exec('rm /tmp/print.txt');
+
+	// Return string
+	$s = '{"success":"true"}';
+	// Return a JSON to account for AJAX print requests as JSONP
+	if (isset($_REQUEST['callback']))
+	{
+		$s = $_REQUEST['callback'].'('.$s.')';	
+	}
+	header("Content-Type: application/json");
+	echo $s;
+?>
